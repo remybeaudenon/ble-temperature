c986b253ce796bd34523205b5945fc1c462c520c
remybeaudenon <remybeaudenon@yahoo.com>
2021-11-28 10:07:09 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.startTemperatureService()
